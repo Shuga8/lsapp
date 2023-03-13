eb72e6b37b0a95944ae7c5284944fc98bce9d238
Shuga8 <lotocharles8@gmail.com>
2023-03-13 12:04:33 +0100

    Commiting...

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{config('app.name', 'LSAPP')}}</title>
+</head>
+<body>
+    
+    <h1>Welcome to laravel</h1>
+    <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+</body>
+</html>
